8b89a70a12f78546fb6939a61107aca8d0f005a0
Mohini <mohinitambade95@gmail.com>
2023-12-29 22:44:49 +0530

    adding model evaluation and mlflow changes

--- src/HealthInsurancePremiumPrediction/pipelines/training_pipeline.py
+++ src/HealthInsurancePremiumPrediction/pipelines/training_pipeline.py
@@ -3,13 +3,19 @@ from src.HealthInsurancePremiumPrediction.logger import logging
 from src.HealthInsurancePremiumPrediction.exception import customexception
 from src.HealthInsurancePremiumPrediction.components.data_transformation import DataTransformation
 from src.HealthInsurancePremiumPrediction.components.model_trainer import ModelTrainer
+from src.HealthInsurancePremiumPrediction.components.model_evaluation import ModelEvaluation
 import pandas as pd
 import os
 import sys
 
 obj=DataIngestion()
 train_data_path,test_data_path = obj.initiate_data_ingestion()
+
 data_transformation=DataTransformation()
 train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
+
+model_evaluation=ModelEvaluation()
+model_evaluation.initiate_model_evaluation(train_arr,test_arr)
